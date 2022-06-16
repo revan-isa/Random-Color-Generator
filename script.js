8a91caabec1ee3f65dfea68fa66b176bc4608a5f
revan-isa <revan.isayev36@gmail.com>
2022-06-16 11:49:44 +0400

    some changes in script [more readable]

--- script.js
+++ script.js
@@ -1,20 +1,17 @@
+let hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "a", "b", "c", "d", "e", "f"];
+
 let btn = document.getElementById("btn");
 let color = document.querySelector(".color");
 
 btn.addEventListener("click", function () {
-  var letters = "0123456789abcdef";
-  var hastag = ["#", "#", "#", "#", "#", "#"];
-
-  for (let i = 0; i < 1; i++) {
-    hastag[i] +=
-      letters[Math.floor(Math.random() * 16)] +
-      letters[Math.floor(Math.random() * 16)] +
-      letters[Math.floor(Math.random() * 16)] +
-      letters[Math.floor(Math.random() * 16)] +
-      letters[Math.floor(Math.random() * 16)] +
-      letters[Math.floor(Math.random() * 16)];
-
-    color.textContent = hastag[i];
-    document.body.style.backgroundColor = hastag[i];
+  var hastag = "#";
+  for (let i = 0; i < 6; i++) {
+    hastag += hex[getRandomNumber()];
   }
+  color.textContent = hastag;
+  document.body.style.backgroundColor = hastag;
 });
+
+function getRandomNumber() {
+  return Math.floor(Math.random() * hex.length);
+}
